b30a797635a183e4bbdd970a013cca09c3c0f25c
Alex Nikanov <alekseynikanov.21@gmail.com>
2023-10-14 17:29:49 -0700

    Get/add a student is working now based on JSON
    Co-authored-by: Will Feldman <willfeldman@users.noreply.github.com>

--- src/api/index.py
+++ src/api/index.py
@@ -1,19 +1,57 @@
-from flask import Flask
-import os
-from dotenv import load_dotenv
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+import bsonjs
+from bson.json_util import dumps as bson_dumps
+from bson.raw_bson import RawBSONDocument
+from bson.objectid import ObjectId
 
 load_dotenv()
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    # Create a new client and connect to the server
-    client = MongoClient(os.getenv('MONGODB_URI'), server_api=ServerApi('1'))
-    # Send a ping to confirm a successful connection
-    try:
-        client.admin.command('ping')
-        return "Pinged your deployment. You successfully connected to MongoDB!"
-    except Exception as e:
-        return e
+# MongoDB connection setup
+client = MongoClient(os.getenv('MONGODB_URI'), server_api=ServerApi('1'), document_class=RawBSONDocument)
+coll = client.ted_ai.students
+
+# Define Student data structure
+class Student:
+    def __init__(self, email, interests, sessions, fname, lname):
+        self.email = email
+        self.interests = interests
+        self.sessions = sessions
+        self.fname = fname
+        self.lname = lname
+
+# API endpoint to add a new student
+@app.route("/api/student/new", methods=["POST"])
+def add_student():
+    data = request.get_json()
+    # email = data.get("email")
+    # interests = data.get("interests")
+    # sessions = data.get("sessions")
+    # fname = data.get("fname")
+    # lname = data.get("lname")
+    
+    # # Create a new student object
+    # new_student = Student(email=email, interests=interests, sessions=sessions, fname=fname, lname=lname)
+
+    bson_bytes = bsonjs.loads(bson_dumps(data))
+    # Insert the new student into the MongoDB collection
+    inserted_id = coll.insert_one(RawBSONDocument(bson_bytes)).inserted_id
+    
+    return jsonify({"message": f"Student added successfully with id { inserted_id }!"}), 201
+
+# API endpoint to get student data by email
+@app.route("/api/student/<id>", methods=["GET"])
+def get_student(id):
+    student_data = coll.find_one({"_id": ObjectId(id)})
+    
+    if student_data:
+        return jsonify(bsonjs.dumps(student_data.raw)), 200
+    else:
+        return jsonify({"message": "Student not found"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
